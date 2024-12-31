82fd8f204068d0a5f68a700b0dc3280a264b22d5
Himanshu-Khairnar <himanshuk1205@gmail.com>
2024-12-31 13:20:37 +0530

    Changes in params for order status

--- src/app/api/orders/[id]/status/route.ts
+++ src/app/api/orders/[id]/status/route.ts
@@ -1,28 +1,26 @@
-import { NextRequest, NextResponse } from 'next/server'; // Import NextRequest and NextResponse
+import {  NextRequest, NextResponse } from 'next/server'; // Import NextRequest and NextResponse
 import connectDB from '@/Database/ConnectDB';
 import { Order } from '@/Models/OrderPlace.model';
+import { useParams } from 'next/navigation'
 
-connectDB(); // Connect to the database
-
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
-    const { id } = params;
-
+connectDB();
+export async function PUT(req: NextRequest) {
+    const params = useParams<{ id:string}>()
+    const {id} = params
     try {
-        const body = await req.json(); // Parse the request body
-        const { newStatus } = body; // Extract newStatus from the request body
+        const body = await req.json(); 
+        const { newStatus } = body; 
 
-        // Find and update the order
         const updatedOrder = await Order.findByIdAndUpdate(id, { status: newStatus }, { new: true });
 
-        // If the order is not found
         if (!updatedOrder) {
             return NextResponse.json({ message: 'Order not found' }, { status: 404 });
         }
 
-        // Return the updated order
         return NextResponse.json(updatedOrder, { status: 200 });
     } catch (error) {
         console.error('Error updating order status:', error);
         return NextResponse.json({ message: 'Error updating order status', error }, { status: 500 });
     }
 }
+

8ec1d075e00d58c95c0ad97fa44c8441eb40498d
Himanshu-Khairnar <himanshuk1205@gmail.com>
2024-12-31 13:01:37 +0530

    resolving issue regard order status patch

--- src/app/api/orders/[id]/status/route.ts
+++ src/app/api/orders/[id]/status/route.ts
@@ -1,31 +1,41 @@
 import connectDB from '@/Database/ConnectDB';
 import { Order } from '@/Models/OrderPlace.model';
-import { NextResponse } from 'next/server';
-import { NextRequest } from 'next/server'; // Import NextRequest for proper typing
+import { NextRequest, NextResponse } from 'next/server';
 
 connectDB();
 
-export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
-    const { id } = params;
+export async function PATCH(
+    req: NextRequest,
+    context: { params: { id: string } }
+) {
+    const { id } = context.params;
 
     try {
-        const body = await req.json(); // Get the request body
-        const { newStatus } = body; // Extract the newStatus from the body
-
+        const body = await req.json();
+        const { newStatus } = body;
         console.log(newStatus);
 
-        // Update the order status
-        const updatedOrder = await Order.findByIdAndUpdate(id, { status: newStatus }, { new: true });
+        const updatedOrder = await Order.findByIdAndUpdate(
+            id,
+            { status: newStatus },
+            { new: true }
+        );
 
-        // Handle the case when the order is not found
         if (!updatedOrder) {
-            return NextResponse.json({ message: 'Order not found' }, { status: 404 });
+            return NextResponse.json(
+                { message: 'Order not found' },
+                { status: 404 }
+            );
         }
 
         console.log(updatedOrder);
         return NextResponse.json(updatedOrder, { status: 200 });
+
     } catch (error) {
         console.error('Error updating order status:', error);
-        return NextResponse.json({ message: 'Error updating order status', error }, { status: 500 });
+        return NextResponse.json(
+            { message: 'Error updating order status', error },
+            { status: 500 }
+        );
     }
 }
